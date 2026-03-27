85424fba6ee7ac95d8e552b996f8babaa93993c7
Bannatte <32626837+Bannatte@users.noreply.github.com>
2026-03-26 23:36:30 -0700

    Rename dRate to rate for clarity

--- main.cpp
+++ main.cpp
@@ -9,22 +9,22 @@ int main()
     const double RATE10 = 3.70;
     const double RATE20 = 4.80;
 
-    double weight, distance, dRate, price;
+    double weight, distance, rate, price;
 
     cin >> weight >> distance;
 
     if (weight <= 0 || weight > 20) return 0;
     if (distance < 10 || distance > 3000) return 0;
 
-    if (weight <= 2) dRate = RATE2;
-    else if (weight <= 6) dRate = RATE6;
-    else if (weight <= 10) dRate = RATE10;
-    else dRate = RATE20;
+    if (weight <= 2) rate = RATE2;
+    else if (weight <= 6) rate = RATE6;
+    else if (weight <= 10) rate = RATE10;
+    else rate = RATE20;
 
     if (distance > 500)
-        price = (distance / 500.0) * dRate;
+        price = (distance / 500.0) * rate;
     else
-        price = dRate;
+        price = rate;
 
     cout << fixed << setprecision(2);
     cout << "The shipping price for package is " << price << endl;
